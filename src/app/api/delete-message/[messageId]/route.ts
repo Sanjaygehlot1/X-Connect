16ec53b7b5a754b1bf51bf1684d353d46cadf5a6
Sanjaygehlot1 <sanjaygehlot1695@gmail.com>
2025-03-02 15:53:42 +0530

    fix: update DELETE API to retrieve messageId from query parameters and add validation for missing messageId

--- src/app/api/delete-message/[messageId]/route.ts
+++ src/app/api/delete-message/[messageId]/route.ts
@@ -4,16 +4,24 @@ import DBConnect from "@/lib/DBConnection";
 import { getServerSession } from "next-auth";
 import { AuthOptions } from "../../auth/[...nextauth]/options";
 import { User } from "next-auth";
-export async function DELETE(request: Request, {params} : {params: {messageId: string}}) {
+export async function DELETE(request: Request) {
 
     DBConnect()
-    const  messageId  =  params?.messageId
+    const { searchParams } = new URL(request.url);
+    const messageId = searchParams.get("messageId");
     const session = await getServerSession(AuthOptions)
     const user: User = session?.user as User
     if (!session || !user) {
         return Response.json(
             { success: false, message: 'Not authenticated' },
             { status: 401 }
+        );
+    }
+
+    if (!messageId) {
+        return Response.json(
+            { success: false, message: "Message ID is required" },
+            { status: 400 }
         );
     }
     
